7a97f131eec6e6da7a84b9a538fd226879ca6d46
Jose Herrera <josehr1108@gmail.com>
2017-06-13 00:43:35 -0600

    cambios home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Coupon;
+use App\Promotion;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -25,6 +26,7 @@ class HomeController extends Controller
     public function index()
     {
         $coupons = Coupon::all()->toJson();
-        return view('home',['cupones' => json_decode($coupons)]);
+        $promotions = Promotion::all()->toJson();
+        return view('home',['cupones' => json_decode($coupons),'promociones' => json_decode($promotions)]);
     }
 }
