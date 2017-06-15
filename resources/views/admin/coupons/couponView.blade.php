85d656ae0d6917fceda87e1a54d0812c6e04e624
Daryn17 <darynsoto22@gmail.com>
2017-06-15 13:20:19 -0600

    Post

--- resources/views/admin/coupons/couponView.blade.php
+++ resources/views/admin/coupons/couponView.blade.php
@@ -87,12 +87,10 @@
     <script>
         function newComent(user, couponId) {
             var userName = user.name;
-            var userEmail = user.email;
             var mensaje = $('#message-text').val();
+            var comment = {'user_name':userName, 'message':mensaje};
 
-                var comment = {'user_name':userName, 'message':mensaje, 'email':userEmail, 'couponId':couponId};
-
-            var url = '/commentCoupon';
+            var url = '/commentCoupon/' + couponId;
             $.ajax({
                 type: "POST",
                 url: url,
