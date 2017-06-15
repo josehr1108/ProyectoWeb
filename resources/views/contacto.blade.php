15e9d8fa3f9205685805835ed427697d4dbfe09e
Jose Herrera <josehr1108@gmail.com>
2017-06-15 13:24:11 -0600

    cambio a postgres

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div></div>
+    <div class="container space">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default shadow">
+                    <div class="panel-heading"><b>Contacta con nosotros</b></div>
+                    <div class="panel-body">
+                        {!!Form::open(['route'=>'admin.promotions.store', 'method'=>'POST', 'class'=>'form-horizontal'])!!}
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            {!!Form::label('nombre', 'Nombre:', ['class'=>'col-md-4 control-label'])!!}
+                            <div class="col-md-6">
+                                {!!Form::text('nombre', '', ['class'=>'form-control', 'required'=>'required', 'autofocus'=>'autofocus'])!!}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            {!!Form::label('email', 'Email:', ['class'=>'col-md-4 control-label'])!!}
+                            <div class="col-md-6">
+                                {!!Form::text('email', '', ['class'=>'form-control', 'required'=>'required'])!!}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            {!!Form::label('mensaje', 'Mensaje:', ['class'=>'col-md-4 control-label'])!!}
+                            <div class="col-md-6">
+                                {!!Form::textarea('mensaje', '', ['class'=>'form-control', 'required'=>'required'])!!}
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                {!!Form::submit('Enviar', ['class'=>'btn button'])!!}
+                            </div>
+                        </div>
+                        {!!Form::close()!!}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
