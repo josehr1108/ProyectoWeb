c6c92713822e45f01f514c869fe800e48319d993
Jose Herrera <josehr1108@gmail.com>
2017-06-14 23:15:26 -0600

    facebook redirect problem

--- config/services.php
+++ config/services.php
@@ -38,6 +38,6 @@ return [
     'facebook' => [
         'client_id' => '1826690634325275',
         'client_secret' => '0662b2a81d013a3cff2af8ade545f5d4',
-        'redirect' => 'https://quiet-fjord-93755.herokuapp.com//auth/facebook/callback',
+        'redirect' => 'https://quiet-fjord-93755.herokuapp.com/auth/facebook/callback',
     ],
 ];
