51ad9c59d9e46545ea82bcbf92613b0c34686436
Jose Herrera <josehr1108@gmail.com>
2017-06-15 01:48:35 -0600

    cambio a mysql

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_b9d525635721b4b'),
+            'username' => env('DB_USERNAME', 'b1be19ee572aaf'),
+            'password' => env('DB_PASSWORD', '01953266'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
